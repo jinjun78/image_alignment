991ed6a9f90ab7c343121df17501d4e0ae0ad1a6
jinjun78 <c0066915@newcastle.ac.uk>
2021-04-30 12:21:39 +0100

    Align images 07 and 08

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import cv2
 
 
 # This script is for alignment of R channel of "P2 SOO1" and B channel of "P2 S002"
-image1 = "P2 S001.png"
-image2 = "P2 S002.png"
+image1 = "P2 S007.jpg"
+image2 = "P2 S008.jpg"
 
 if __name__ == '__main__':
-    refFilename = Split(image1).save_r()  #reference image
-    imFilename = Split(image2).save_b()  #image to be aligned
+    refFilename = image1  #reference image
+    imFilename = image2  #image to be aligned
     ref = cv2.imread(refFilename, cv2.IMREAD_COLOR)
     tar = cv2.imread(imFilename, cv2.IMREAD_COLOR)
     print("Reference image : ", refFilename,
