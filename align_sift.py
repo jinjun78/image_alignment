173574c32e8a31b701532e60c2d186ffd0c23bc6
jinjun78 <c0066915@newcastle.ac.uk>
2021-05-12 13:20:56 +0100

    Update images name

--- align_sift.py
+++ align_sift.py
@@ -3,8 +3,8 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-image1 = cv2.imread("sample1.png")
-image2 = cv2.imread("sample2.png")
+image1 = cv2.imread("equ1.png")
+image2 = cv2.imread("equ2.png")
 
 # Convert images to greyscale
 im1_Grey = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
@@ -12,11 +12,11 @@ im2_Grey = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
 
 # Detect ORB features and compute descriptor
 ## Only for comparision
-orb = cv2.ORB_create()
-keypoints1, descriptors1 = orb.detectAndCompute(im1_Grey, None)
-keypoints2, descriptors2 = orb.detectAndCompute(im2_Grey, None)
-image1_o = cv2.drawKeypoints(im1_Grey, keypoints1, None, color=(0,255,0), flags=0)
-image2_o = cv2.drawKeypoints(im2_Grey, keypoints2, None, color=(0,255,0), flags=0)
+# orb = cv2.ORB_create()
+# keypoints1, descriptors1 = orb.detectAndCompute(im1_Grey, None)
+# keypoints2, descriptors2 = orb.detectAndCompute(im2_Grey, None)
+# image1_o = cv2.drawKeypoints(im1_Grey, keypoints1, None, color=(0,255,0), flags=0)
+# image2_o = cv2.drawKeypoints(im2_Grey, keypoints2, None, color=(0,255,0), flags=0)
 
 # Detect SIFT features and compute descriptor
 sift = cv2.SIFT_create()
@@ -28,8 +28,6 @@ image1_s = cv2.drawKeypoints(im1_Grey, kp1, None, color=(0,0,255), flags=0)
 image2_s = cv2.drawKeypoints(im2_Grey, kp2, None, color=(0,0,255), flags=0)
 
 # Show features detected by ORB and SIFT
-plt.imshow(image1_o),plt.show()
-plt.imshow(image2_o),plt.show()
 plt.imshow(image1_s),plt.show()
 plt.imshow(image2_s),plt.show()
 
@@ -48,7 +46,7 @@ for m,n in matches:
 # Draw good matches
 img3 = cv2.drawMatchesKnn(image1, kp1, image2, kp2, good, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
 
-# cv2.imwrite("matches.jpg", img3)
+cv2.imwrite("matches.jpg", img3)
 
 # Extract location of good matches
 points1 = np.float32([kp1[m.queryIdx].pt for m in good_without_list])
@@ -64,4 +62,4 @@ imReg = cv2.warpPerspective(image1, h, (width, height))
 # Save aligned image
 outFilename = "aligned1.jpg"
 print("Saving aligned image: ", outFilename)
-# cv2.imwrite(outFilename, imReg)
+cv2.imwrite(outFilename, imReg)
