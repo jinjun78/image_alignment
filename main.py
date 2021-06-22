8896ba8a18c1de3eb42da6481624ccbc3a0cb04c
jinjun78 <c0066915@newcastle.ac.uk>
2021-06-22 14:52:49 +0100

    Add SIFT alignment and result testing into the workflow

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
-from RGB_split import Split
-import alignment as aln
+import align_orb as aln_olb
+import align_sift as aln_sift
+import test_alignment as test
 import cv2
+from PIL import Image
 
-
-# This script is for alignment of R channel of "P2 SOO1" and B channel of "P2 S002"
-image1 = "sample1.png"
-image2 = "sample2.png"
 
 if __name__ == '__main__':
-    refFilename = image1  #reference image
-    imFilename = image2  #image to be aligned
+    refFilename = "sec1_colour.png" #reference image
+    imFilename = "sec2_colour.png"  #target image
     ref = cv2.imread(refFilename, cv2.IMREAD_COLOR)
     tar = cv2.imread(imFilename, cv2.IMREAD_COLOR)
     print("Reference image : ", refFilename,
           "\nImage to be aligned : ", imFilename)
-    aln.alignImages(ref, tar)
+    # aln_olb.alignORB(ref, tar)
+    align = aln_sift.alignSIFT(ref, tar)
+
+    ref = Image.open(refFilename)
+    tar = Image.open(imFilename)
+    out = Image.open(align)
+    print("Saving alignment quality test image: ", align)
+    test.alignImages(ref,out,0,1)
