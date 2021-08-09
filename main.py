3ddb2eb7744b64c0bfc817b54e653627ebba0df4
jinjun78 <c0066915@newcastle.ac.uk>
2021-08-09 16:54:51 +0100

    Update annotation of the script

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from workflow import Worlflow
 import sys
 
-sys.stdout = open("final_output/log5.txt", "w")
+
+# '''All the output stored in a single text file as named.
+#    Remember to change text file name each time.'''
+sys.stdout = open("final_output/log1.txt", "w")
+
 if __name__ == '__main__':
-    ref = "aligned_4_1000_0.1.jpg"
-    im = "S004.jpg"
-    time = 5
-    Worlflow(ref, im, time)
+    ref = "S001.jpg"  # reference image file name
+    im = "S002.jpg"          # target image file name
+    time = 1                  # the time of alignment
+    Worlflow(ref, im, time)   # automatic workflow of alignment
 
 sys.stdout.close()
